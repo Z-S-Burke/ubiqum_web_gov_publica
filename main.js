2a39f8d5105f75406e0fb23df0d10717c26fef14
Z. S. Burke <zach@zsburke.com>
2019-03-11 13:13:06 +0100

    removed unnecessary console.log functions

--- main.js
+++ main.js
@@ -5,29 +5,21 @@ function memberTable(allMembers) {
 
     for (var i = 0; i < allMembers.length; i++) {
         var tRow = tBody.insertRow("tRow");
-//        var cellText = tRow.insertCell(0);
-        //for (var i2 = 0; i2 < 1; i2++) {
+        tRow.insertCell().innerHTML = i + 1 + ".";
         if (allMembers[i].middle_name == null) {
             tRow.insertCell().innerHTML =
                 allMembers[i].last_name + ", " + allMembers[i].first_name;
-            console.log(allMembers[i].last_name + ", " + allMembers[i].first_name);
         } else {
             tRow.insertCell().innerHTML = allMembers[i].last_name + ", " + allMembers[i].first_name + " " + allMembers[i].middle_name;
-            console.log(allMembers[i].last_name + ", " + allMembers[i].first_name + " " + allMembers[i].middle_name);
         }
         tRow.insertCell().innerHTML = allMembers[i].party;
-        console.log(allMembers[i].party);
 
         tRow.insertCell().innerHTML = allMembers[i].state;
-        console.log(allMembers[i].state);
 
         tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-        console.log(allMembers[i].seniority);
 
-        tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
-        console.log(allMembers[i].votes_with_party_pct);
-        
-        tRow.appendChild; 
+        tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";        
+        tBody.append(tRow);
     }
 }
 
