ba2b7a88a4e16a869c41c7af3537ed40a3a73ccd
Z. S. Burke <zach@zsburke.com>
2019-03-12 15:05:01 +0100

    added e-mail logo in header and an accordion element to the home page for expanded information on TGIF

--- main.js
+++ main.js
@@ -36,3 +36,15 @@ function memberTable(allMembers) {
 }
 
 memberTable(allMembers);
+
+/*
+function() {
+    $('#accordion').on('shown.bs.collapse', function (e) {
+        var offset = $(this).find('.collapse.in').prev('.panel-heading');
+        if(offset) {
+            $('html,body').animate({
+                scrollTop: $(offset).offset().top -50
+            }, 500); 
+        }
+    }); 
+});*/
