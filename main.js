92328f95d22ef82ed513d507e09c0b39ab934b64
Z. S. Burke <zach@zsburke.com>
2019-03-13 16:22:50 +0100

    created a function that receives value when checkbox is interacted with, to soon be used with JSON table creation

--- main.js
+++ main.js
@@ -2,36 +2,40 @@ var allMembers = data.results[0].members; //allows us to access just the members
 
 function memberTable(allMembers) {
     var tBody = document.getElementById("tBody");
+    if (document.querySelectorAll('input[type=checkbox]:checked') == true) {
+    
+    } else {
+        for (var i = 0; i < allMembers.length; i++) {
 
-    for (var i = 0; i < allMembers.length; i++) {
-        var tRow = tBody.insertRow("tRow");
-        tRow.insertCell().innerHTML = i + 1 + ".";
+            var tRow = tBody.insertRow("tRow");
+            tRow.insertCell().innerHTML = i + 1 + ".";
 
-        if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
-            var repName =
-                allMembers[i].last_name + ", " + allMembers[i].first_name;
-            tRow.insertCell().innerHTML =
-                repName.link(allMembers[i].url); //uses the result to hyperlink the string 
-        } else {
-            var repName =
-                allMembers[i].last_name +
-                ", " + allMembers[i].first_name +
-                " " + allMembers[i].middle_name;
-            tRow.insertCell().innerHTML =
-                repName.link(allMembers[i].url); //uses the result to hyperlink the string
+            if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
+                var repName =
+                    allMembers[i].last_name + ", " + allMembers[i].first_name;
+                tRow.insertCell().innerHTML =
+                    repName.link(allMembers[i].url); //uses the result to hyperlink the string 
+            } else {
+                var repName =
+                    allMembers[i].last_name +
+                    ", " + allMembers[i].first_name +
+                    " " + allMembers[i].middle_name;
+                tRow.insertCell().innerHTML =
+                    repName.link(allMembers[i].url); //uses the result to hyperlink the string
+            }
+
+            tRow.insertCell().innerHTML = allMembers[i].party;
+            tRow.insertCell().innerHTML = allMembers[i].state;
+
+            if (allMembers[i].seniority == 1) {
+                tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
+            } else {
+                tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
+            }
+
+            tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
+            tBody.append(tRow);
         }
-
-        tRow.insertCell().innerHTML = allMembers[i].party;
-        tRow.insertCell().innerHTML = allMembers[i].state;
-
-        if (allMembers[i].seniority == 1) {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-        } else {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-        }
-
-        tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
-        tBody.append(tRow);
     }
 }
 
@@ -39,7 +43,10 @@ memberTable(allMembers);
 
 function partySelect() {
     var partyNode = document.querySelectorAll('input[type=checkbox]:checked');
-    
     var partyArr = Array.from(partyNode);
-    // return partyArr; 
+    
+    for (i = 0; i < partyArr.length; i++) {
+        console.log(partyArr[i].value);
+    }
+
 }
