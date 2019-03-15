1edf70350429417f48799a731e2399fd739aa67e
Z. S. Burke <zach@zsburke.com>
2019-03-15 13:09:51 +0100

    placed buildRow into a seperate function to dramatically shorten buildTable code length

--- main.js
+++ main.js
@@ -16,174 +16,79 @@ stateDrop();
 
 function memberTable(allMembers) {
     var tBody = document.getElementById("tBody");
-
     var partyNode = document.querySelectorAll('input[type=checkbox]:checked'); //finds the values of all checkboxes on the page, places into a node
-
     var partyArr = Array.from(partyNode); //converts the node into an Array type variable 
-
     var dropdown = document.getElementById("state");
 
-    console.log('selectedIndex', dropdown.selectedIndex)
-    console.log('options', dropdown.options)
-    console.log('selected')
-    console.log(dropdown.selectedIndex);
-    console.log(usStates[dropdown.selectedIndex].abbreviation)
-
-    console.log(partyArr.length > 0);
-
-    console.log(allMembers[0].state, usStates[dropdown.selectedIndex].abbreviation);
-
     if (dropdown.selectedIndex != 0 && partyArr.length > 0) {
-        console.log('first if')
-        
+
         tBody.innerHTML = "";
         tBody.insertRow("tRow");
         for (var i = 0; i < allMembers.length; i++) {
             for (var j = 0; j < partyArr.length; j++) {
                 if (allMembers[i].party == partyArr[j].value &&
                     allMembers[i].state == usStates[dropdown.selectedIndex].abbreviation) {
-        
-                    var tRow = tBody.insertRow();
-                    
-                    tBody.insertRow("tRow");
-
-                    tRow.insertCell().innerHTML = i + 1 + ".";
-
-                    if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
-                        var repName =
-                            allMembers[i].last_name + ", " + allMembers[i].first_name;
-                        tRow.insertCell().innerHTML =
-                            repName.link(allMembers[i].url); //uses the result to hyperlink the string 
-                    } else {
-                        var repName =
-                            allMembers[i].last_name +
-                            ", " + allMembers[i].first_name +
-                            " " + allMembers[i].middle_name;
-                        tRow.insertCell().innerHTML =
-                            repName.link(allMembers[i].url); //uses the result to hyperlink the string
-                    }
-
-                    tRow.insertCell().innerHTML = allMembers[i].party;
-                    tRow.insertCell().innerHTML = allMembers[i].state;
-
-                    if (allMembers[i].seniority == 1) {
-                        tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-                    } else {
-                        tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-                    }
-
-                    tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
+                    buildRow(allMembers[i]);
                 }
             }
         }
     } else if (partyArr.length > 0) {
-        console.log('else if 1')
         tBody.innerHTML = "";
         tBody.insertRow("tRow");
         for (var i = 0; i < allMembers.length; i++) {
             for (var j = 0; j < partyArr.length; j++) {
                 if (allMembers[i].party == partyArr[j].value) {
-
-                    var tRow = tBody.insertRow();
-
-                    tRow.insertCell().innerHTML = i + 1 + ".";
-
-                    if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
-                        var repName =
-                            allMembers[i].last_name + ", " + allMembers[i].first_name;
-                        tRow.insertCell().innerHTML =
-                            repName.link(allMembers[i].url); //uses the result to hyperlink the string 
-                    } else {
-                        var repName =
-                            allMembers[i].last_name +
-                            ", " + allMembers[i].first_name +
-                            " " + allMembers[i].middle_name;
-                        tRow.insertCell().innerHTML =
-                            repName.link(allMembers[i].url); //uses the result to hyperlink the string
-                    }
-
-                    tRow.insertCell().innerHTML = allMembers[i].party;
-                    tRow.insertCell().innerHTML = allMembers[i].state;
-
-                    if (allMembers[i].seniority == 1) {
-                        tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-                    } else {
-                        tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-                    }
-
-                    tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
+                    buildRow(allMembers[i]);
                 }
             }
         }
     } else if (dropdown.selectedIndex != 0) {
-        console.log('else if 2')
         tBody.innerHTML = "";
-        tBody.insertRow("tRow");        
+        tBody.insertRow("tRow");
         for (var i = 0; i < allMembers.length; i++) {
             if (allMembers[i].state == usStates[dropdown.selectedIndex].abbreviation) {
-                
-                var tRow = tBody.insertRow();
-                
-                tRow.insertCell().innerHTML = i + 1 + ".";
-
-                if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
-                    var repName =
-                        allMembers[i].last_name + ", " + allMembers[i].first_name;
-                    tRow.insertCell().innerHTML =
-                        repName.link(allMembers[i].url); //uses the result to hyperlink the string 
-                } else {
-                    var repName =
-                        allMembers[i].last_name +
-                        ", " + allMembers[i].first_name +
-                        " " + allMembers[i].middle_name;
-                    tRow.insertCell().innerHTML =
-                        repName.link(allMembers[i].url); //uses the result to hyperlink the string
-                }
-
-                tRow.insertCell().innerHTML = allMembers[i].party;
-                tRow.insertCell().innerHTML = allMembers[i].state;
-
-                if (allMembers[i].seniority == 1) {
-                    tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-                } else {
-                    tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-                }
-
-                tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
+                buildRow(allMembers[i]);
             }
         }
     } else {
-    console.log('else')
-    tBody.innerHTML = "";
-    for (var i = 0; i < allMembers.length; i++) {
-        tBody.insertRow("tRow");
-        var tRow = tBody.insertRow();
-        tRow.insertCell().innerHTML = i + 1 + ".";
-        if (allMembers[i].middle_name == null) { //accounts for members w/o middle names
-            var repName =
-                allMembers[i].last_name + ", " + allMembers[i].first_name;
-            tRow.insertCell().innerHTML =
-                repName.link(allMembers[i].url); //uses the result to hyperlink the string 
-        } else {
-            var repName =
-                allMembers[i].last_name +
-                ", " + allMembers[i].first_name +
-                " " + allMembers[i].middle_name;
-            tRow.insertCell().innerHTML =
-                repName.link(allMembers[i].url); //uses the result to hyperlink the string
+        tBody.innerHTML = "";
+        for (var i = 0; i < allMembers.length; i++) {
+        buildRow(allMembers[i]);
         }
-
-        tRow.insertCell().innerHTML = allMembers[i].party;
-        tRow.insertCell().innerHTML = allMembers[i].state;
-
-        if (allMembers[i].seniority == 1) {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-        } else {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-        }
-
-        tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
     }
 }
-}
 memberTable(allMembers);
+
+
+function buildRow(allMembers) {
+    var tBody = document.getElementById("tBody");
+    var tRow = tBody.insertRow();
+
+    tBody.insertRow("tRow");
+    tRow.insertCell().innerHTML =  allMembers.title;
+
+    if (allMembers.middle_name == null) { //accounts for members w/o middle names
+        var repName =
+            allMembers.last_name + ", " + allMembers.first_name;
+        tRow.insertCell().innerHTML =
+            repName.link(allMembers.url); //uses the result to hyperlink the string 
+    } else {
+        var repName =
+            allMembers.last_name +
+            ", " + allMembers.first_name +
+            " " + allMembers.middle_name;
+        tRow.insertCell().innerHTML =
+            repName.link(allMembers.url); //uses the result to hyperlink the string
+    }
+
+    tRow.insertCell().innerHTML = allMembers.party;
+    tRow.insertCell().innerHTML = allMembers.state;
+
+    if (allMembers.seniority == 1) {
+        tRow.insertCell().innerHTML = allMembers.seniority + " year";
+    } else {
+        tRow.insertCell().innerHTML = allMembers.seniority + " years";
+    }
+
+    tRow.insertCell().innerHTML = allMembers.votes_with_party_pct + "%";
+}
